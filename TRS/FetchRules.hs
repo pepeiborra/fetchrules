514ff150460874444f801e498e2988d0d07e4365
pepe <pepe@ender4.dsic.upv.es>
2008-06-21 18:39:29 +0200

    Update to TRS 0.13
    darcs-hash:20080621163929-26539-a14d8f8220d30002377889b9c3be045c533f5e6d.gz

--- TRS/FetchRules.hs
+++ TRS/FetchRules.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE TypeOperators, PolymorphicComponents, PatternSignatures, GADTs #-}
+{-# LANGUAGE PatternGuards, TypeOperators, PolymorphicComponents, PatternSignatures, GADTs #-}
 {-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleContexts, TypeSynonymInstances #-}
-module TRS.FetchRules (parseFile, parseFileAndTerms, ParseProgram(..), FetchRules(..), parsers) where
+module TRS.FetchRules (parseFile, parseFileAndTerms, parse, ParseProgram(..), FetchRules(..), parsers) where
 
 import Control.Applicative
 import Control.Monad.Error
@@ -8,33 +8,37 @@ import Data.AlaCarte
 import Data.List (elemIndex, nub, partition)
 import Data.Generics
 import Data.Maybe
+import Data.Monoid
 import System.FilePath
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (parse)
 
 import TRS
 
-import OBJ_parser as OBJ (OBJProgram(..), Term(..), EqD(..), TermD, parseOBJ, parseTerm)
+import OBJ_parser as OBJ (OBJProgram(..), Term(..), EqD(..), TermD, St,object,anyTerm)
 import qualified TTTParser as TTT
 import qualified TRSParser as TRSP
 import qualified TRSTypes as TRST
 
 
-parseFile :: (Var :<: t, T :<: t) =>
-             [Char] -> String -> Either ParseError [Rule t]
+-- TODO: Remove the term parsing functionality from the library
+--       I do no longer believe in its usefulness
+
+parseFile :: (Var :<: t, T String :<: t) =>
+             FilePath -> String -> Either ParseError [Rule t]
 parseFile fn contents = msum(map tryParser guessedParsers) `mplus`
                    tryParser (head guessedParsers)  -- For error displaying
     where tryParser (SomeParser p) = fmap fetchRules (parseP p fn contents)
           guessedParsers = sortParsers fn
 
-parseFileAndTerms :: (Var :<: t, T :<: t) =>
-             [Char] -> String -> [String] -> Either ParseError ([Rule t ], [TRS.Term t])
+parseFileAndTerms :: (Var :<: t, T String :<: t) =>
+             FilePath -> String -> [String] -> Either ParseError ([Rule t ], [TRS.Term t])
 parseFileAndTerms fn contents terms =
            msum(map tryParser guessedParsers) `mplus`
            tryParser (head guessedParsers)  -- For error displaying
     where tryParser (SomeParser p) = do
             program <- parseP p fn contents
             terms   <- mapM (parseT p contents) terms
-            return (fetchRules program, fetchTerm program <$> terms)
+            return (fetchRules program, fetchTerm_ program <$> terms)
           guessedParsers = sortParsers fn
 
 sortParsers :: FilePath -> [SomeParser]
@@ -47,47 +51,54 @@ proxy = undefined :: Proxy a
 
 type FullProgram = String
 
-class ParseProgram program term error | program -> term error
-                                      , term    -> program error
- where   grammar      :: Proxy program  -> String
-         parseProgram :: FilePath -> FullProgram -> Either error program
-         parseTerm    :: FullProgram  -> String -> Either error term
+class Monoid s => ParseProgram program term s | program -> term s
+                                              , term    -> program s
+ where   grammar  :: Proxy program  -> String
+         programP :: GenParser Char s program
+         termP    :: GenParser Char s term
          needsProgramToParseTerm :: Proxy program -> Bool
 
-parseP :: ParseProgram program t e => Proxy program -> (FilePath -> FullProgram -> Either e program)
-parseP _ = parseProgram
+parseP :: ParseProgram program t s => Proxy program -> (FilePath -> FullProgram -> Either ParseError program)
+parseP _ = parse programP
 
-parseT :: ParseProgram p t e => Proxy p -> (FullProgram -> String -> Either e t)
-parseT _ = TRS.FetchRules.parseTerm
+parseT :: ParseProgram p t s => Proxy p -> (FullProgram -> String -> Either ParseError t)
+parseT _ = parse TRS.FetchRules.termP
 
-instance ParseProgram TTT.TRS TTT.Term ParseError where
-   grammar _    = "ttt"
-   parseProgram = parse TTT.tttParser
-   parseTerm _  = parse TTT.term "<expr>"
+parse p = runParser p mempty
+
+instance ParseProgram TTT.TRS TTT.Term () where
+   grammar _ = "ttt"
+   programP  = TTT.tttParser
+   termP     = TTT.term
    needsProgramToParseTerm _ = False
 
-instance ParseProgram OBJProgram TermD ParseError where
-   grammar _      = "maude"
-   parseProgram _ = parseOBJ
-   parseTerm      = OBJ.parseTerm
+instance ParseProgram OBJProgram TermD OBJ.St where
+   grammar _ = "maude"
+   programP  = OBJ.object
+   termP     = OBJ.anyTerm
    needsProgramToParseTerm _ = True
 
-instance ParseProgram  TRST.Spec TRST.Term ParseError where
-   grammar _    = "trs"
-   parseProgram = parse TRSP.trsParser
-   parseTerm _  = parse TRSP.term "<expr>"
+instance ParseProgram  TRST.Spec TRST.Term () where
+   grammar _ = "trs"
+   programP  = TRSP.trsParser
+   termP     = TRSP.term
    needsProgramToParseTerm _ = False
 
 data SomeParser where
-  SomeParser ::  forall p t. (ParseProgram p t ParseError, FetchRules p t) => Proxy p -> SomeParser
+  SomeParser ::  forall p t s. (ParseProgram p t s, FetchRules p t) => Proxy p -> SomeParser
 
 parsers = [SomeParser (proxy :: Proxy TTT.TRS)
           ,SomeParser (proxy :: Proxy OBJProgram)
           ,SomeParser (proxy :: Proxy TRST.Spec)]
 
 class FetchRules program term | program -> term, term -> program where
-  fetchRules :: (Var :<: t, T :<: t) => program -> [Rule t]
-  fetchTerm  :: (Var :<: t, T :<: t) => program -> term -> TRS.Term t
+  fetchRules :: (Var :<: t, T String:<: t) => program -> [Rule t]
+  fetchTerm_ :: (Var :<: t, T String :<: t) => program -> term -> TRS.Term t
+  fetchTerm_ _ = fetchTerm
+  needProgramToFetchTerm   :: Proxy program -> Bool
+  needProgramToFetchTerm _ = True
+  fetchTerm  ::  (Var :<: t, T String :<: t) => term -> TRS.Term t
+  fetchTerm  = error "Use fetchTerm_ for this parser"
 
 instance FetchRules OBJProgram TermD where
   fetchRules (Obj _ _ _ obj_vars obj_eqs) = map fromOBJEq obj_eqs
@@ -95,7 +106,7 @@ instance FetchRules OBJProgram TermD where
             fromOBJTerm (Call f tt) = term f (map fromOBJTerm tt) 
             vars = [ name | (name,_sort) <- obj_vars ]
             fromOBJEq (Eq t1 t2) = fromOBJTerm t1 :-> fromOBJTerm t2
-  fetchTerm (Obj _ _ _ obj_vars _obj_eqs) = fromOBJTerm
+  fetchTerm_ (Obj _ _ _ obj_vars _obj_eqs) = fromOBJTerm
       where fromOBJTerm (OBJ.Var sym) | Just i <- elemIndex sym vars = var i
             fromOBJTerm (Call f tt) = term f (map fromOBJTerm tt) 
             vars = [ name | (name,_sort) <- obj_vars ]
@@ -111,7 +122,8 @@ instance FetchRules TTT.TRS TTT.Term where
          all_vars = nub$ concatMap vars (listify (\(_::TTT.Term)->True) rr)
          isTTTVar TTT.V{} = True
          isTTTVar _ = False
-  fetchTerm rr t = termTTT t
+  fetchTerm t = termTTT t
+  needProgramToFetchTerm _ = False
 
 termTTT t = fromTerm t where
          fromTerm (TTT.V n) | Just i <- elemIndex n all_vars = var i
@@ -127,7 +139,7 @@ instance FetchRules TRST.Spec TRST.Term where
          mkTerm (TRST.T id tt) 
              | Just i <- id `elemIndex` vars = var i
              | otherwise      = term id (mkTerm `map` tt)
-  fetchTerm  (TRST.Spec decls) = mkTerm
+  fetchTerm_ (TRST.Spec decls) = mkTerm
    where vars = concat [v | TRST.Var v <- decls]
          mkTerm (TRST.T id tt) 
              | Just i <- id `elemIndex` vars = var i
