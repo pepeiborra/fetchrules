697309f7b8d9c942ecbe4c2dfe0395ab57e55668
pepe <pepe@ender4.dsic.upv.es>
2008-10-22 11:46:54 +0200

    improve error handling in Auto
    darcs-hash:20081022094654-26539-2c0011304d3513a1599802346e8e5ce3ec1e8a73.gz

--- instances/Auto/TRS/FetchRules/Auto.hs
+++ instances/Auto/TRS/FetchRules/Auto.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE FlexibleContexts, TypeOperators, PolymorphicComponents, GADTs #-}
-module TRS.FetchRules.Auto (SomeParser(..), parseFileAuto, parseFileAndTermsAuto) where
+module TRS.FetchRules.Auto (SomeParser(..), parseFileAuto, parseFileAndTermsAuto, mapError, bestError) where
 
 import Control.Applicative
 import Control.Monad.Error
 import Data.AlaCarte
-import Data.List (elemIndex, nub, partition)
+import Data.List (elemIndex, nub, partition, maximumBy)
 import Data.Monoid
 import System.FilePath
 import Text.ParserCombinators.Parsec hiding (parse)
+import Text.ParserCombinators.Parsec.Error
 import TRS
 import TRS.FetchRules
 
+instance Error a => Error [a] where
+  noMsg    = [noMsg]
+  strMsg t = [strMsg t]
 
 data SomeParser where
   SomeParser ::  forall p t s. (ParseProgram p t s, FetchRules p t) => Proxy p -> SomeParser
@@ -21,18 +25,18 @@ parsers = [SomeParser (proxy :: Proxy TTT.TRS)
           ,SomeParser (proxy :: Proxy TRST.Spec)]
 -}
 parseFileAuto :: (Var :<: t, T String :<: t) =>
-             [SomeParser] -> FilePath -> String -> Either ParseError [Rule t]
-parseFileAuto parsers fn contents = msum(map tryParser guessedParsers) `mplus`
-                   tryParser (head guessedParsers)  -- For error displaying
-    where tryParser (SomeParser p) = fmap fetchRules (parseP p fn contents)
+             [SomeParser] -> FilePath -> String -> Either [ParseError] [Rule t]
+parseFileAuto parsers fn contents = mapError atom (msum attempts) `mplus` throwError [err | Left err <- attempts]
+    where attempts = map tryParser guessedParsers
+          tryParser (SomeParser p) = fmap fetchRules (parseP p fn contents)
           guessedParsers = sortParsers parsers fn
 
-parseFileAndTermsAuto :: (Var :<: t, T String :<: t) =>
-             [SomeParser] -> FilePath -> String -> [String] -> Either ParseError ([Rule t ], [TRS.Term t])
+parseFileAndTermsAuto :: (Var :<: t, T String :<: t, Var :<: u, T String :<: u) =>
+             [SomeParser] -> FilePath -> String -> [String] -> Either [ParseError] ([Rule t ], [TRS.Term u])
 parseFileAndTermsAuto parsers fn contents terms =
-           msum(map tryParser guessedParsers) `mplus`
-           tryParser (head guessedParsers)  -- For error displaying
-    where tryParser (SomeParser p) = do
+           mapError atom (msum attempts) `mplus` throwError [err | Left err <- attempts]
+    where attempts = map tryParser guessedParsers
+          tryParser (SomeParser p) = do
             program <- parseP p fn contents
             terms   <- mapM (parseT p contents) terms
             return (fetchRules program, fetchTerm_ program <$> terms)
@@ -51,3 +55,12 @@ parseT _ = parse TRS.FetchRules.termP
 
 parse p = runParser p mempty
 
+
+atom x = [x]
+
+mapError f (Left e)  = Left (f e)
+mapError f (Right x) = Right x
+
+bestError :: [ParseError] -> ParseError
+bestError = maximumBy (compare `on` errorPos)
+ where on cmp f x y = f x `cmp` f y
